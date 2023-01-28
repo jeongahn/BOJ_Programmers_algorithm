8614be8dcb2fa458e244e18535ba01d6d5bdc80c
jeongahn <inetty@naver.com>
2023-01-28 14:30:21 +0900

    done:)

--- BOJ/String/2671.py
+++ BOJ/String/2671.py
@@ -1,2 +1,61 @@
-# baekjoon 14503 gold5 로봇 청소기
+# baekjoon 2671 gold5 잠수함식별
 # 23년 상반기 Foscar 알고리즘 스터디 5조 1주차 2번
+
+
+# 1번 re 모듈 사용
+# import re
+
+# sound = input()
+
+# pattern = re.compile('(100+1+|01)+')
+
+# result = pattern.fullmatch(sound)
+
+# if result:
+#     print("SUBMARINE")
+# else:
+#     print("NOISE")
+
+# 2번 구현
+
+sound = input()
+
+def check():
+    i = 0
+    s = len(sound)
+
+    while(i < s):
+        if sound[i] == '0': # 01부터 시작하면
+            if(i + 1 >= s):
+                return False # 최소 길이 2이므로 01
+            if(sound[i+1] != '1'):
+                return False
+            i += 2 # 01이면 인덱스 2개를 봤으므로 다음부터는 2번 인덱스부터 확인
+        else:
+            if(i+3 >= s):
+                return False #1 00 1 최소 길이 4
+            if(not(sound[i+1] == '0' and sound[i+2] == '0')):
+                return False
+            i += 1
+            while(i < s and sound[i] == '0'):
+                i += 1 #1 00 1에서 1을 지나고 마지막 1이 오기전 계속 0이 올 때
+
+            if(i >= s):
+                return False # 1 00~ 0이 계속 오다가 결국 1이 오지 않으면 noise
+            i+=1 #결국 1 00 다음 1이 오면
+            while(i < s and sound[i] == '1'):
+                if(i + 2 < s and sound[i+1] == '0' and sound[i+2] == '0'):
+                    break # 1 00~ 1~이기 때문에 1 00 1 00 패턴이 올 수 있다 그런 경우 다시 위에서 부터 순회
+                i+=1    
+    return True
+
+
+def solution():
+    if(check()):
+        print("SUBMARINE")
+    else:
+        print("NOISE")
+
+solution()        
+        
+
---
+++ BOJ/brute_force/16637.py
@@ -0,0 +1,54 @@
+'''
+3+8*7-9*2
+
+1. (3+8)*(7-9)*2 = -96
+2. (3+8)*7-9*2 = 136
+4. (3+8)*7-(9*2) = 59
+5. 3+(8*7)-(9*2) = 41
+6. 3+(8*7)-9*2 = 41
+7. 3+8*(7-9)*2 = 
+8. 3+8*7-(9*2)
+9. 3+8*7-9*2
+
+
+DFS로 괄호를 묶은것과 괄호를 묶지 않은 것으로 구분지어서 재귀함수
+
+
+'''
+
+length = int(input())
+express = input()
+answer = float('-inf')
+
+
+#괄호를 묶을때 연산해주기 위한 함수
+def cal(oper, a, b):
+    if oper == '+':
+        return a + b
+    elif oper == '-':
+        return a - b
+    elif oper == '*':
+        return a * b
+
+def DFS(idx, val):
+
+    global answer
+
+    if idx >= length:
+        answer = max(answer, val)
+        return
+    
+    if idx == 0:
+        oper = '+'
+    else:
+        oper = express[idx-1]
+
+    if idx + 2 < length:
+        tmp = cal(express[idx + 1], int(express[idx]), int(express[idx+2]))
+        DFS(idx+4, cal(oper, val, tmp)) 
+
+    DFS(idx + 2, cal(oper, val, int(express[idx])))
+
+
+DFS(0,0)
+print(answer)
